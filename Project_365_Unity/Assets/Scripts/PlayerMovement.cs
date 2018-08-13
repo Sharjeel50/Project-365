5c5863f42ff508ddf18e4ec1a0ffaa0d437c9bf8
Sharjeel50 <41446219+Sharjeel50@users.noreply.github.com>
2018-08-13 19:46:45 +0100

    2nd Commit

--- Project_365_Unity/Assets/Scripts/PlayerMovement.cs
+++ Project_365_Unity/Assets/Scripts/PlayerMovement.cs
@@ -6,46 +6,40 @@ public class PlayerMovement : MonoBehaviour {
 
 
     public float speed = 6f;
-    public float gravity = -9.8f;
+    public float gravity = 150f;
     private CharacterController _charCont;
 
-    private Rigidbody rb;
-    public float JumpForce = 7f;
-    public GameObject bullet;
-    public CapsuleCollider col;
-
+    public float JumpForce = 70f;
     public LayerMask groundLayers;
 
-	// Use this for initialization
-	void Start () {
+   // private Vector3 moveDirection = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
         _charCont = GetComponent<CharacterController>();
-        col = GetComponent<CapsuleCollider>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		float deltaX = Input.GetAxis("Horizontal") * speed; //Get the inputs from 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed; //Get the inputs from 
         float deltaY = Input.GetAxis("Vertical") * speed;
         Vector3 Movement = new Vector3(deltaX, 0, deltaY);
         Movement = Vector3.ClampMagnitude(Movement, speed); //clamps speed so movement is always the same
 
-        Movement.y = gravity; // sets gravity on y axis
+
+        if (_charCont.isGrounded && Input.GetButton("Jump"))
+        {
+            Movement.y += JumpForce;
+        }
+        else {
+             Movement.y -= gravity * Time.deltaTime;
+        }
 
         Movement *= Time.deltaTime; // fixes the movement so that its the same across different frame rates.
         Movement = transform.TransformDirection(Movement);
         _charCont.Move(Movement);
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-        }
-    
-	}
-
-    private bool IsGrounded()
-    {
-
-        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
-            col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
     }
 
 }
