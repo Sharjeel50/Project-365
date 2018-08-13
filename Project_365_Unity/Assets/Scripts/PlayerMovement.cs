895ee07a3072826671a861452ca23031895bce33
tamuk <tamuka120>
2018-08-13 19:55:24 +0100

    Latest Commit

--- Project_365_Unity/Assets/Scripts/PlayerMovement.cs
+++ Project_365_Unity/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,9 @@ public class PlayerMovement : MonoBehaviour {
 
 
     public float speed = 6f;
-    public float gravity = -9.8f;
+    public float gravity = 20.0f;
     private CharacterController _charCont;
+    public bool jumpkey;
 
 	// Use this for initialization
 	void Start () {
@@ -15,16 +16,43 @@ public class PlayerMovement : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float deltaX = Input.GetAxis("Horizontal") * speed; //Get the inputs from 
-        float deltaY = Input.GetAxis("Vertical") * speed;
-        Vector3 Movement = new Vector3(deltaX, 0, deltaY);
+        float deltaZ = Input.GetAxis("Vertical")* speed;
+
+        Vector3 Movement = new Vector3(deltaX, 0, deltaZ);
         Movement = Vector3.ClampMagnitude(Movement, speed); //clamps speed so movement is always the same
 
-        Movement.y = gravity; // sets gravity on y axis
+        //Jump Below
+
+        if (Input.GetButton("Jump"))
+        {
+            jumpkey = true;
+        } else
+        {
+            jumpkey = false;
+        }
+
+        if (_charCont.isGrounded && jumpkey == true)
+        {
+            while (jumpkey == true)
+            {
+                Movement.y += 2.0f;
+            }
+            
+        }
+        else if (_charCont.isGrounded)
+        {
+            Movement.y = 0.0f;
+        }
+        else
+        {
+            Movement.y -= gravity * Time.deltaTime;
+        }
+
 
         Movement *= Time.deltaTime; // fixes the movement so that its the same across different frame rates.
         Movement = transform.TransformDirection(Movement);
-        _charCont.Move(Movement);
+        _charCont.Move(Movement); 
 	}
 }
