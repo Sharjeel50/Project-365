172dc4525773509840c0188397aecfbaff2ef310
tamuk <tamuka120>
2018-08-15 16:17:12 +0100

    Shooting And Physics
    Adding RayCast Damage Mechanic
    Mesh Colliders for Buildings

---
+++ Project_365_Unity/Assets/Scripts/Gun and Damage/GunScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            TreeDamage target = hit.transform.GetComponent<TreeDamage>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
